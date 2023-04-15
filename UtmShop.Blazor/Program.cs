144a72fcd95b12ca3a57e0a576313924b3c741c6
Sam Rutcovschi <samuil.rutcovschi@codwer.com>
2023-04-15 20:13:30 +0300

    Add page for categories

--- UtmShop.Blazor/Program.cs
+++ UtmShop.Blazor/Program.cs
@@ -6,6 +6,12 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+var defaultUri = new Uri(builder.HostEnvironment.BaseAddress);
+var utmShopApiUri = new Uri("https://localhost:5001/");
+
+builder.Services.AddScoped(sp => new HttpClient
+{
+    BaseAddress = utmShopApiUri
+});
 
 await builder.Build().RunAsync();
